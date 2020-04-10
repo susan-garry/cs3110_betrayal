10302ebf4ba4b5bfce7e14f7265a1d6abe5af070
Daphne Rios <dr434@cornell.edu>
2020-04-09 17:27:32 -0700

    commit on 'my_branch'

--- cards.ml
+++ cards.ml
@@ -1,6 +1,7 @@
 type card_type = Item | Omen | Event
-type card_effect = {e_description: string; e_effects: string option}
+type card_effect = {e_description: string; e_effects: string list option}
 type card = {c_type: card_type; name: string; description: string; effects: card_effect}
+type card_stack = {items: card list list; omens: card list; events: card list}
 
 exception UnknownCardEffect of card_effect
 exception UnknownCard of card
