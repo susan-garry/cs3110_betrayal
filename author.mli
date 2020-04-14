3f47c5f241941a245c7b52a35e10860efa161328
Susan H Garry <shg64@cornell.edu>
2020-04-14 16:47:01 -0400

    Delete author.mli

--- author.mli
+++
@@ -1,7 +0,0 @@
-(**
-   Metadata about this submission.
-   @author Daphne Rios (dr434), Susan Garry (), & Isabel Selin (). 
-*)
-
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
