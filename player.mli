907dfd95e7fbf8d16c4b33c0ad84641007d0f7c1
Susan Garry <shg64@cornell.edu>
2020-04-19 17:35:37 -0400

    Update state.mli and player.mli

--- player.mli
+++ player.mli
@@ -1,1 +1,16 @@
+module type Player = sig
+  type t
 
+  type might
+  type speed
+  type knowledge
+  type sanity
+
+  (**[move p t] returns a player identical to [p] but located in [t]*)
+  val move : player : Tile.t -> player
+
+  (**[location p] returns the tile where [player] is currently located*)
+  val location : player -> Tile.t
+
+  (**[get_] *)
+end
--- state.mli
+++ state.mli
@@ -1,1 +1,21 @@
+open Player
+open Tile
 
+module type State = sig
+
+  (**The abstract type for values representing a game state *)
+  type t
+
+  (**[from_json json] takes a json file and creates the initial game state*)
+  val from_json : Yojson.Basic.t -> t
+
+  (**[move_player player tile state] moves [player] to [tile], regardless
+     of whether or not [tile] is accessible from [player]'s current location. 
+     Throws [] if tile is not Tile of Room*)
+  val move_player : Player.t -> Room.t -> t -> t
+
+  (**[add_room room exit state] adds a room to the board from [exit] of [room].
+  Throws [] if a room already exists through that exit. *)
+  val add_room : Room.t -> Room.exit -> t -> t
+
+end
