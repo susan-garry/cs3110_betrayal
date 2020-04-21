3b140584fd84918096f53d38325c46d02365c6e3
Daphne Rios <dr434@cornell.edu>
2020-04-21 12:21:43 -0700

    commit on 'master'

--- gui.ml
+++ gui.ml
@@ -1,1 +1,8 @@
+open ANSITerminal
+(* open Command *)
+
+
+let start_screen = print_string [red]"\n\nWelcome to Betrayal of CU on the Hill! \n"
+
+let prompt = print_endline "> ";
 
--- test.ml
+++ test.ml
@@ -1,25 +1,25 @@
 open OUnit2
-open Cards
 open Rooms
-open House
-
-let cards_tests = [
-
-]
+open Command
+open State
 
 let rooms_tests = [
 
 ]
 
-let house_tests = [
+let commands_tests = [
+
+]
+
+let state_tests = [
 
 ]
 
 let suite =
   "test suite for A2"  >::: List.flatten [
-    cards_tests;
     rooms_tests;
-    house_tests;
+    commands_tests;
+    state_tests;
   ]
 
 let _ = run_test_tt_main suite
