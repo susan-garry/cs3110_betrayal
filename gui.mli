2a19647624cdea1ddc32354b203da88c9ef3dd67
Daphne Rios <dr434@cornell.edu>
2020-04-21 14:04:36 -0700

    commit on 'master'

--- gui.mli
+++ gui.mli
@@ -1,5 +1,9 @@
 (* Gives user prompts, takes user commands as input *)
 
 (* Will likely use ASCII representation for displaying the map of the board and user interface 
+    __ __ _____
+   |     |     |
+    4 x 2   1
+   |__ __|_____|
 
 *)
