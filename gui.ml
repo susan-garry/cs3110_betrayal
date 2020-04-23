9900b95964c77253ae49b010a4d3e5110c37faf0
Daphne Rios <dr434@cornell.edu>
2020-04-23 12:50:12 -0700

    Update gui.ml

--- gui.ml
+++ gui.ml
@@ -6,3 +6,20 @@ let start_screen = print_string [red]"\n\nWelcome to Betrayal of CU on the Hill!
 
 let prompt = print_endline "> ";
 
+type gui_tile = {top:string; middle: string; bottom: string}
+
+let parse_a_tile = {
+  top = " _____ "; 
+  middle = "|     |";
+  bottom = "|_____|"
+}
+
+let print_a_tile r = 
+  print_endline r.top;
+  print_endline r.middle;
+  print_endline r.bottom
+
+(** print " _____ \n|     |\n|     | \n|_____|" *)
+
+(** print " _____ _____ _____ _____ _____ _____\n|     |     |     |     |     |     |\n|     |     |     |     |     |     |\n|_____|_____|_____|_____|_____|_____|" *)
+
