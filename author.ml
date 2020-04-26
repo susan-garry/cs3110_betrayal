25c23a12d2e9f61333c8d093d1aa90613a6ad4a7
Susan Garry <shg64@cornell.edu>
2020-04-25 21:46:04 -0400

    Hours updates in author.ml and debugging comments removed from State.move_player

--- author.ml
+++ author.ml
@@ -1,6 +1,6 @@
 (* TODO: set the value below *)
-let daphne_hours = 0
-let isabel_hours = 0
-let susan_hours = 9
+let daphne_hours = 10
+let isabel_hours = 16
+let susan_hours = 16
 
 let hours_worked = [daphne_hours; isabel_hours; susan_hours]
--- state.ml
+++ state.ml
@@ -163,17 +163,11 @@ let move_player (dir:Command.direction) state =
         in
         let t_coord = Tiles.get_coords tile in 
         let f_coord = first_coord s' in
-        if fst t_coord = fst f_coord then 
-          (        print_endline "western edge";
-                   print_int (fst t_coord);
-                   print_int (fst f_coord);
-                   add_w_row tile s') else 
+        if fst t_coord = fst f_coord then
+          add_w_row tile s' else 
         if snd t_coord = snd f_coord 
         then 
-          (print_endline "northern edge";
-           print_int (fst t_coord);
-           print_int (fst f_coord);
-           add_n_row tile s') else
+          add_n_row tile s' else
         if fst t_coord + 1 = fst f_coord + s'.x_dim then s' |> add_e_row tile 
         else
         if tile |> Tiles.get_s |> snd = None then s' |> add_s_row tile
