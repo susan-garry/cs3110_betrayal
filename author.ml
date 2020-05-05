91c0675485b9e7f84e8ce2bd46b33c9bf11e9fae
Susan Garry <shg64@cornell.edu>
2020-05-05 00:51:36 -0400

    Update player.ml to implement status

--- author.ml
+++ author.ml
@@ -1,6 +1,6 @@
 (* TODO: set the value below *)
 let daphne_hours = 10
 let isabel_hours = 16
-let susan_hours = 16.5
+let susan_hours = 17.5
 
 let hours_worked = [daphne_hours; isabel_hours; susan_hours]
--- player.ml
+++ player.ml
@@ -1,41 +1,20 @@
 open OUnit2
 type player_id = int
 type player_name = string
-<<<<<<< HEAD
-type stat = int
-=======
+type status = int
+
 type player_stats = {speed:int; might:int; sanity:int; knowledge:int}
->>>>>>> c1c9789b7d6be2acf4a0f51c5ecb9c05a6c17dfd
 
 type t = { name : player_name;
            location : Tiles.t;
-<<<<<<< HEAD
-           might : stat;
-           speed : stat;
-           sanity : stat;
-           knowledge : stat}
-=======
-           stats: player_stats;
-           next_player : t option ref}
->>>>>>> c1c9789b7d6be2acf4a0f51c5ecb9c05a6c17dfd
+           stats: player_stats }
 
 exception LastPlayer
 
 let empty = { name = "Player 1";
               location = Tiles.empty;
-<<<<<<< HEAD
-              might = 4;
-              speed = 4;
-              sanity = 4;
-              knowledge = 4}
-=======
-              stats = {speed=4; might=4; sanity=4; knowledge=4};
-              next_player = ref None}
-
-let get_id p = p.id
-
-let set_id id p = {p with id = id}
->>>>>>> c1c9789b7d6be2acf4a0f51c5ecb9c05a6c17dfd
+              stats = {speed=4; might=4; sanity=4; knowledge=4}
+            }
 
 let get_name p = p.name
 
@@ -45,12 +24,6 @@ let get_loc p = p.location
 
 let move t p = {p with location = t}
 
-<<<<<<< HEAD
-=======
-let get_next p = !(p.next_player)
-
-
->>>>>>> c1c9789b7d6be2acf4a0f51c5ecb9c05a6c17dfd
 (*-------------------------------------------*)
 (*Code for testing here*)
 
