628e20bd8b8a4e77e9e8fae81b1c883563958d60
Susan Garry <shg64@cornell.edu>
2020-05-06 00:08:46 -0400

    Implement add_player and get_status
    Note that get_status currently returns the empty list because state
    needs to update the win/loss status of players

--- player.mli
+++ player.mli
@@ -14,7 +14,7 @@ val empty : t
 (**[get_name p] returns the name of [p]*)
 val get_name : t -> string
 
-(**[set_id n p] returns a player identical to [p] but whose name is [n]*)
+(**[set_name n p] returns a player identical to [p] but whose name is [n]*)
 val set_name : string -> t -> t
 
 (**[player_location p] returns the tile where [player] is currently located*)
@@ -29,11 +29,11 @@ val set_stat : player_stats -> string -> int -> player_stats
 
 (** [player_lose p count] is true if one of the player's stats is less than or equal to [count]. 
     Otherwise, false *)
-val player_lose : t -> int -> bool
+val player_lose : t -> bool
 
 (** [player_win p count] is true if one of the player's stats is greater than or euqal to [count]. 
     Otherwise, false. *)
-val player_win : t -> int -> bool
+val player_win : t -> bool
 
 (** [print_player p] returns unit; printing out the name, location, and stats of player [p]. *)
 val print_player : t -> unit
