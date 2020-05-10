72350711ac6221ba49593194dfa3a9fd1d16f179
Daphne Rios <dr434@cornell.edu>
2020-05-09 19:26:31 -0700

    commit on 'matser'

--- main.ml
+++ main.ml
@@ -58,11 +58,17 @@ let parse_move d state =
     print_newline ();
     state
 
-let check_status = ()
-
+let check_status st = 
+  begin match State.get_status st with 
+    | [] -> ()
+    | h::t -> 
+      begin match h with 
+        | Win s -> ()
+        | Lose s -> ()
+      end
+  end
 (** [play st] resumes play from the game state in [state]. *)
 let rec play state = 
-
   print_endline (State.room_desc state);
   print_newline ();
   print_string "It is "; print_string (State.player_desc state); 
