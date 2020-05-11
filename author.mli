4954b4c486f6221502a401fd580903baf53df77c
Susan Garry <shg64@cornell.edu>
2020-05-11 02:39:52 -0400

    Revert "Remove author module; authors are credited in the specific files which they edited"
    This reverts commit fbbd0cca43383911699a9c2c2a699a6ace790159.

---
+++ author.mli
@@ -0,0 +1,7 @@
+(**
+   Metadata about this submission.
+   @author Susan Garry (shg64), Daphne Rios (dr434), Isabel Selin. 
+*)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int list
