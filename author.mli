75b44911184a67ba2a99133c7dd41d27b59adc3d
Susan Garry <shg64@cornell.edu>
2020-05-11 02:44:37 -0400

    Authors individual contributions listed in authors.mli

--- author.mli
+++ author.mli
@@ -1,6 +1,12 @@
-(**
-   Metadata about this submission.
-   @author Susan Garry (shg64), Daphne Rios (dr434), Isabel Selin. 
+(** @author Susan Garry (shg64)
+               Files: state.ml, state.mli, player.ml, player.mli, main.ml,
+                  gui.ml, gui.mli
+            Isabel Selin (is389)
+               Files: rooms.ml, rooms.mli, tiles.ml, tiles.mli, effects.ml,
+                  effects.mli, state.ml, spooky_game.json
+            Daphne Rios (dr434)
+               Files: gui.ml, gui.mli, main.ml, players.ml, players.mli,
+                  command.ml, command.mli, spooky_game.json
 *)
 
 (** [hours_worked] is the number of hours you worked on this assignment. *)
